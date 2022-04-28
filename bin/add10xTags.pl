4792a6b902bd2ad93bf625e4a81eff71ef16df2e
Stefan Lang <Stefan.Lang@med.lu.se>
2022-04-28 10:39:52 +0200

    Added the functional part of the script...

--- bin/add10xTags.pl
+++ bin/add10xTags.pl
@@ -1,1 +1,38 @@
-add10xTags.pl
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use SingelCellSAM;
+
+#sam/bam ifile, opath, source, taget
+
+my ( $R2, $I1, $barcodes ) = @ARGV;
+
+my $usage =
+"samtools view <bam> | add10xTags.pl <R2.fastq.gz> <I1.fastq.gz> <barcodes.tsv> > <annotated.sam>
+
+Where bam, R2.fastq.gz and I1.fastq.gz are input files.
+The R1 and possibly R3 reads should have been mapped to a genome 
+and the sam entries of that genome should be piped through this tool.
+
+The barcodes.tsv file would contain the sorted cell barcodes and read counts per barcode.
+The modified bam reads are written to STDOUT.
+
+"
+
+unless ( -f $R2 ){
+	die ( $usage )
+}
+
+unless ( -f $I1 ){
+	die ( $usage )
+}
+
+if ( undef $barcodes ){
+	die ( $usage )
+}
+
+my $object = SingelCellSAM->new ();
+
+$object->annotate10xcells( \*STDIN , $FASTQ, $I1, $barcodes );
+
